df1246cbc761a5e2039dd082bc6f6f199d94af8e
Venu Sirpuram <sirpuram10@gmail.com>
2020-09-21 17:00:10 +0530

    Deployed my first react Todo List app

---
+++ build/precache-manifest.21c2b4ac19dd73c880ff094efb034b5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7794bf627eec4c744c3d797b736d9cc3",
+    "url": "/reacttodolist/index.html"
+  },
+  {
+    "revision": "cfb8a53b1abce0a29320",
+    "url": "/reacttodolist/static/css/main.78ce9836.chunk.css"
+  },
+  {
+    "revision": "918cb0d3f7a799517214",
+    "url": "/reacttodolist/static/js/2.310780bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reacttodolist/static/js/2.310780bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfb8a53b1abce0a29320",
+    "url": "/reacttodolist/static/js/main.e3e490be.chunk.js"
+  },
+  {
+    "revision": "25052f3892efac266391",
+    "url": "/reacttodolist/static/js/runtime-main.d58f725a.js"
+  }
+]);
